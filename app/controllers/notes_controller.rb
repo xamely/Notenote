cb389be3c7bc6baf48a48935129118d9e1b8ca1a
xamely <m.xeniya.a@ya.ru>
2018-12-26 22:54:21 +0300

    Edit pages designed

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,7 @@
 class NotesController < ApplicationController
   before_action :find_note, only: [:show, :edit, :update, :destroy]
   def index
-    @notes = Note.where(user_id: current_user)
+    @notes = Note.where(user_id: current_user).order("created_at DESC")
   end
 
   def show
